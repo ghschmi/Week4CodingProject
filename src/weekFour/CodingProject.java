6ac4a61e9e2b80a1e7187c4f20e9777692f9d243
ghschmi <ghschmi9@gmail.com>
2024-12-07 23:23:46 -0500

    updates

--- src/weekFour/CodingProject.java
+++ src/weekFour/CodingProject.java
@@ -3,7 +3,6 @@ package weekFour;
 public class CodingProject {
 	public static void main(String[] args) {
 		
-		
 	/*
 	 * STEP 1
 	 */
@@ -21,7 +20,6 @@ public class CodingProject {
 		}
 		double averageAge = (double) sum / ages2.length; //this divides the sum by the number of elements 
 		System.out.println(averageAge); //output is 31.9 which is the average of all the elements
-		
 		
 	/*
 	 * STEP 2
@@ -46,7 +44,6 @@ public class CodingProject {
 	        }
 	        
 	       System.out.println(concatenatedNames);  // the output is: Sam Tommy Tim Sally Buck Bob
-	
 	       
 	/*
 	 * STEP 3
@@ -54,7 +51,6 @@ public class CodingProject {
 	 * The last element will be 1 less than length
 	 * example: array[array.length - 1];
 	 */
-		
 	       
 	/*
 	 * STEP 4
@@ -74,11 +70,11 @@ public class CodingProject {
 	       
 	       for (int i = 0; i < nameLengths.length; i++) {    //using another for loop to iterate through the nameLengths array and print the result
 	    	   System.out.println(names[i] + " " + nameLengths[i]);   //output gives each name and their corresponding length separated by a space
-	       }
-	       
+	       } 
 	       
 	/*
 	 * STEP 6
+	 * calculates sum
 	 */
 	       int step6 = 0;  // initializing a variable for the sum 
 	       
@@ -89,7 +85,8 @@ public class CodingProject {
 	       System.out.println(step6);  //output is 23 which is the sum of all the name lengths 
 	            
 	/*
-	 * STEP 7            
+	 * STEP 7
+	 * (method) concatenates a string a specified amount of times            
 	 */   
 	       String word1 = "Hello";  //creating a String named word
 	       int n = 3;    //creating an int called n
@@ -103,18 +100,18 @@ public class CodingProject {
 	   		System.out.println(step7Method);  //output is HelloHelloHello but word and n inputs can be changed to alter output 
 	}
 	
-	
 	/*
-	 * STEP 8            
+	 * STEP 8
+	 * (method) returns string with first and last name            
 	 */
 	 	    public static String getFullName(String firstName, String lastName) {  //created a method with the strings firstName and lastName
 	 	    	
 	 		return firstName + " " + lastName;  // returns the concatenated first and last name with a space in between
-	    }
-	            
+	    }          
 	
 	 /*
 	  * STEP 9
+	  * (method) checks if sum of elements in an array are greater than 100
 	  */
 	        public static boolean step9Method(int[] step9Array) { //initialization of method that takes an array of integers
 	        int sum = 0;    //creating variable to hold sum
@@ -122,11 +119,11 @@ public class CodingProject {
 	        		sum += num;    //calculates sum of the integers in the array
 	        }
 	        return sum > 100;  //will return true if the sum is greater than 100
-	    }
-	    
-	        
+	    }   
+	       
 	 /*
 	  * STEP 10
+	  * (method) calculates avg of elements within a double array
 	  */
 	        public static double step10Method(double[] step10Array) {  //initialization of method that takes an array of double
 	        	double sum = 0;  //creating variable to hold sum
@@ -136,11 +133,11 @@ public class CodingProject {
 	        		
 	        	}
 	        	return sum / step10Array.length;   //divides the sum by the length to return the average 
-	        }
-	        
+	        }      
 	        
 	  /*
 	   * STEP 11
+	   * (method) compares avg of two double arrays 
 	   */
 	        public static boolean step11Method(double[] array1, double[] array2) { //initialization of method that takes two arrays of double
 	        	double avg1 = step10Method(array1);    //calling the method from step 10 to calculate the average
@@ -148,6 +145,25 @@ public class CodingProject {
 	        	
 	        	return avg1 > avg2;   //returns true if the average of array1 is greater than that of array2
 	        }
-	    
-	
+	        
+	 /*
+	  * STEP 12
+	  * (method) checks conditions to determine whether or not to buy a drink
+	  */
+	        public static boolean willBuyDrink(boolean isHotOutside, double moneyInPocket) { //initialization of method that takes a boolean and a double
+	        	
+	        	return isHotOutside && moneyInPocket > 10.50; // if both isHotOutside and moneyInPocket is greater than 10.50, will return true; otherwise, returns false
+	        }
+	 /*
+	  * STEP 13
+	  * creating my own method that calculates price after sales 
+	  */
+	      //init of method that takes the initial price of the item and the percentage of the sale
+	        public static double calculateSalePrice(double initialPrice, double percentOff) {
+	        
+	        double saleTotal = (initialPrice * percentOff) / 100; //calculating the amount that the sale deducts
+	        double salePrice = initialPrice - saleTotal;  //subtracting the amount the sale deducts from the initial price to get the final sale amount
+	        
+	        return salePrice;  //will return the final sale price
+	        }
 }
